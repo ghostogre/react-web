ccdb05254c7b50d8240720736cbd7ea256a6be46
yingjh <yjh.1234@qq.com>
2019-07-13 18:19:29 +0800

    init

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import shopList from './components/shopList'
+
+function App() {
+  return (
+    <div className="App">
+      <shopList/>
+    </div>
+  );
+}
+
+export default App;
